30d4b31969bd475cf41fbe86b85e71eb10008c2c
Patrick Conner <PConner000@gmail.com>
2017-10-18 09:56:03 -0600

    Added MainMenuTests.cs      -tests that "Exit" menu button exits the app      -tests that "New Game" shifts to a gameplay screen Removed TestStack.White from packages      -UI automation framework Added FlaUI to packages      -UI automation framework Refactored UnitTest1.cs and UnitTest2.cs to have useful names (PlayerStatsTests.cs and StonesTests.cs, not necessarily respectively) Refactored ClassLauncher.cs to TestResources.cs to reflect its new application as a collection of static resources for testing      -I.E. Relative path to executable for UI testing

---
+++ Pente/Pente-Testing/MainMenuTests.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+//using TestStack.White;
+//using TestStack.White.UIItems.WindowItems;
+//using TestStack.White.UIItems;
+//using TestStack.White.UIItems.ListBoxItems;
+//using TestStack.White.UIItems.Finders;
+using FlaUI.Core;
+using FlaUI.UIA3;
+using FlaUI.Core.AutomationElements;
+using FlaUI.Core.AutomationElements.Infrastructure;
+using System.Threading;
+
+namespace Pente_Testing {
+    [TestClass]
+    public class MainMenuTests {
+        [TestMethod]
+        public void TestExit() {
+            Application app = Application.Launch(TestResources.ApplicationPath);
+            using (UIA3Automation automation = new UIA3Automation()) {
+                Window window = app.GetMainWindow(automation);
+                Button but = window.FindFirstDescendant(x => x.ByName("Exit")).AsButton();
+
+
+                but.Click();
+
+                bool fail = true;
+                try {
+                    window.Click();
+                }catch{
+                    fail = false;
+                }
+
+                Assert.IsFalse(fail);
+            }
+        }
+        [TestMethod]
+        public void TestGameStart_basic() {
+            Application app = Application.Launch(TestResources.ApplicationPath);
+            using (UIA3Automation automation = new UIA3Automation()) {
+                Window window = app.GetMainWindow(automation);
+                Button but = window.FindFirstDescendant(x => x.ByAutomationId("NewGameButton")).AsButton();
+
+                but.Click();
+                
+                AutomationElement board = window.FindFirstDescendant(x => x.ByClassName("GameScreen"));
+
+                Assert.IsNotNull(board);
+            }
+        }
+    }
+}
